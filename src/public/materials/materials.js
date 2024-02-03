23d292f38a84401553f3540254784d59870fe93c
frunkers <frunkers@users.noreply.github.com>
2024-02-03 18:45:55 +0000

    Deploying to gh-pages from @ Frintest/endclatum@de9c1a101c15a3a09605126dc1028fd636dbaf1c 🚀

--- src/public/materials/materials.js
+++
@@ -1,6 +0,0 @@
-import '../js/app.js';
-import '../scss/main.scss';
-import '../scss/components/general/general.scss';
-import '../scss/components/materials/materials.scss';
-
-import favicon from '../img/favicon.svg';
